6a93824ca2ea46228130fe1accbec641fe9786fe
Bernardo Vieira <bernardo5@live.com.pt>
2019-12-09 11:25:07 +0000

    we stopped trackying types folder. See #25

--- types/ganache-time-traveler/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'ganache-time-traveler' {
-    export const advanceTime: any;
-    export const advanceBlock: any;
-    export const advanceBlockAndSetTime: any;
-    export const advanceTimeAndBlock: any;
-    export const takeSnapshot: any;
-    export const revertToSnapshot: any;
-}
