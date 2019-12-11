e8256766857619af75bc622dd61aad0c2ce98267
Alberto Cuesta Cañada <mail@albertocuesta.es>
2019-12-11 15:58:15 +0000

    IssuanceSimple.sol drafted..

---
+++ contracts/drafts/issuance/IERC20Mintable.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.10;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP. Does not include
+ * the optional functions; to access them see {ERC20Detailed}.
+ */
+interface IERC20Mintable {
+    /**
+     * @dev See {ERC20-_mint}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the {MinterRole}.
+     */
+    function mint(address account, uint256 amount)
+        external
+        returns(bool);
+}
