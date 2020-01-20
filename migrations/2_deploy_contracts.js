bcfaea4516f20a36fd867129272d750e55fa9fe7
Bernardo Vieira <bernardo5@live.com.pt>
2020-01-20 08:35:48 +0000

    clean coverage

--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const StringConversion = artifacts.require('./StringConversion.sol');
-const RBAC = artifacts.require('./access/RBAC.sol');
-
-module.exports = async (deployer, network, accounts) => {
-    deployer.deploy(StringConversion);
-    deployer.deploy(RBAC, accounts[0]);
-};
