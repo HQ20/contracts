a8fa676e631ef3c88417264844dd10eba7dec310
vsfalliance <>
2020-02-13 12:50:57 +0200

    Rename reruie messages, rename quorum to threshold, add thresholdVotes function

--- contracts/drafts/voting/Voting.sol
+++ contracts/drafts/voting/Voting.sol
@@ -14,11 +14,11 @@ import "../../utils/SafeCast.sol";
  * @dev Implements a simple voting process for proposals
  *
  * 1. Initialize the Voting with the votingToken address and the address and the callData of the proposal you wish to enact, should this Voting pass.
- * 2. Setup the Voting by providing a quorum. The quorum must be expressed as an integer between 1 and 10000, representing a double digit percentage of the total supply of the voting tokens, with the comma shifted two digits to the right.
+ * 2. Setup the Voting by providing a threshold. The threshold must be expressed as an integer between 1 and 10000, representing a double digit percentage of the total supply of the voting tokens, with the comma shifted two digits to the right.
  * 3. Open the voting.
  * 4. Cast votes.
  * 5. You can cancel your vote at any time. You should cancel it after the voting proposal has passed or after the voting has been cancelled.
- * 6. Validate the quorum.
+ * 6. Validate the threshold.
  * 7. Enact the proposal.
  */
 contract Voting is Ownable, StateMachine {
@@ -28,7 +28,7 @@ contract Voting is Ownable, StateMachine {
     using SafeCast for uint256;
 
     event VotingCreated();
-    event QuorumSet();
+    event ThresholdSet();
     event VoteCasted(address voter, uint256 votes);
     event VoteCancelled(address voter, uint256 votes);
 
@@ -39,7 +39,7 @@ contract Voting is Ownable, StateMachine {
     address[] public voters;
     mapping(address => uint256) public votes;
 
-    uint256 public quorum;
+    uint256 public threshold;
 
     /**
      * @dev Initialize the issuance with the token to issue and the token to
@@ -65,15 +65,15 @@ contract Voting is Ownable, StateMachine {
     /**
      * @dev Function to enact the proposal of this voting
      */
-    function enact() external payable onlyOwner {
+    function enact() external payable {
         require(
             currentState == "PASSED",
-            "Cannot enact proposal now."
+            "Cannot enact proposal until vote passes."
         );
         // solium-disable-next-line security/no-call-value
         (bool success, ) = proposalContract
             .call.value(msg.value)(proposalData);
-        require(success, "Could not enact proposal.");
+        require(success, "Failed to enact proposal.");
     }
 
     /**
@@ -99,10 +99,6 @@ contract Voting is Ownable, StateMachine {
      */
     function cancel() external {
         require(
-            currentState != "SETUP",
-            "Cannot retrieve now."
-        );
-        require(
             votes[msg.sender] > 0,
             "No votes casted."
         );
@@ -117,25 +113,19 @@ contract Voting is Ownable, StateMachine {
      */
     function open() public onlyOwner {
         require(
-            quorum > 0,
-            "Quorum not set."
+            threshold > 0,
+            "Threshold not set."
         );
         _transition("OPEN");
     }
 
     /**
-     * @dev Function to validate the quorum
+     * @dev Function to validate the threshold
      */
     function validate() public onlyOwner {
-        ERC20Detailed _votingToken = ERC20Detailed(votingToken);
-        int256 totalSupplyFixed = _votingToken.totalSupply()
-            .safeUintToInt().newFixed(_votingToken.decimals());
-        int256 quorumFixed = quorum.safeUintToInt().newFixed(2);
-        uint256 threshold = totalSupplyFixed.multiply(quorumFixed)
-            .fromFixed(_votingToken.decimals()).safeIntToUint();
         require(
-            _votingToken.balanceOf(address(this)) >= threshold,
-            "Not enough votes to meet the quorum."
+            IERC20(votingToken).balanceOf(address(this)) >= thresholdVotes(),
+            "Not enough votes to meet the threshold."
         );
         _transition("PASSED");
     }
@@ -148,20 +138,29 @@ contract Voting is Ownable, StateMachine {
     }
 
     /**
-     * @notice Set the quorum during SETUP state. Quorum must be a percentage of the votingToken's totalSupply, with the comma shifted two digits to the right. For example:
-     * If desired quorum is 100% (unanimity), then pass to this function: 10000.
-     * If desired quorum is 66.67% (super majority), then passs to this function: 6667.
+     * @notice Set the threshold during SETUP state. Threshold must be a percentage of the votingToken's totalSupply, with the comma shifted two digits to the right. For example:
+     * If desired threshold is 100% (unanimity), then pass to this function: 10000.
+     * If desired threshold is 66.67% (super majority), then passs to this function: 6667.
      */
-    function setQuorum(uint256 _quorum) public onlyOwner {
+    function setThreshold(uint256 _threshold) public onlyOwner {
         require(
             currentState == "SETUP",
             "Cannot setup now."
         );
         require(
-            _quorum > 0 && _quorum <= 10000,
+            _threshold > 0 && _threshold <= 10000,
             "Quroum must be a percentage."
         );
-        quorum = _quorum;
-        emit QuorumSet();
+        threshold = _threshold;
+        emit ThresholdSet();
+    }
+
+    function thresholdVotes() internal returns (uint256) {
+        ERC20Detailed _votingToken = ERC20Detailed(votingToken);
+        int256 totalSupplyFixed = _votingToken.totalSupply()
+            .safeUintToInt().newFixed(_votingToken.decimals());
+        int256 thresholdFixed = threshold.safeUintToInt().newFixed(2);
+        return totalSupplyFixed.multiply(thresholdFixed)
+            .fromFixed(_votingToken.decimals()).safeIntToUint();
     }
 }
