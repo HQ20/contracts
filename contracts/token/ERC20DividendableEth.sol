96bc3c10df89cecbdcecb74ed0b56dfcc47facc5
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-02-21 12:27:14 +0000

    Added comments to fix Fixidity conversions.

--- contracts/token/ERC20DividendableEth.sol
+++ contracts/token/ERC20DividendableEth.sol
@@ -14,13 +14,13 @@ import "../utils/SafeCast.sol";
 contract ERC20DividendableEth is ERC20MintableDetailed {
 
     using SafeMath for uint256;
-    using FixidityLib for int256;
     using SafeCast for uint256;
     using SafeCast for int256;
+    using FixidityLib for int256;
 
-    uint256 public dividendsPerToken;
+    uint256 public dividendsPerToken; // This should be a Fixidity fixed point number
 
-    mapping(address => uint256) public lastDividendsPerToken;
+    mapping(address => uint256) public lastDividendsPerToken; // This should be a Fixidity fixed point number
 
     constructor(
         string memory name,
@@ -43,12 +43,12 @@ contract ERC20DividendableEth is ERC20MintableDetailed {
         return claimDividends(msg.sender);
     }
 
+    // Specify in natspec this takes an amount in wei.
     function releaseDividends(uint256 amount) internal {
-        int256 fixedSupply = this.totalSupply()
-            .safeUintToInt();
-        int256 fixedValue = amount.safeUintToInt().newFixed();
+        int256 fixedSupply = this.totalSupply().safeUintToInt(); // This needs to be converted from ether/wei to Fixidity with .newFixed(this.decimals())
+        int256 fixedValue = amount.safeUintToInt().newFixed(); // This needs to be converted from ether/wei to Fixidity with .newFixed(this.decimals())
         uint256 releasedDividends = fixedValue
-            .divide(fixedSupply).fromFixed(this.decimals()).safeIntToUint();
+            .divide(fixedSupply).fromFixed(this.decimals()).safeIntToUint(); // dividendsPerToken is a Fixidity number, so no need to convert back.
         dividendsPerToken = dividendsPerToken.add(releasedDividends);
     }
 
@@ -66,14 +66,16 @@ contract ERC20DividendableEth is ERC20MintableDetailed {
      * @dev Internal function to compute dividends owing to an account
      * @param account The account for which to compute the dividends
      */
+    // Specify in natspec this returns an amount in wei.
+    // Let's make sure that we have numerical tests for this one, not only 100% overage. I'll think of a test suite.
     function dividendsOwing(address account) internal view returns(uint256) {
         uint256 owedDividendsPerToken = dividendsPerToken
             .sub(lastDividendsPerToken[account]);
         int256 fixedBalance = this.balanceOf(account)
-            .safeUintToInt();
+            .safeUintToInt(); // This needs to be converted from ether/wei to Fixidity with .newFixed(this.decimals())
         int256 fixedOwed = owedDividendsPerToken
-            .safeUintToInt().newFixed(this.decimals());
-        return fixedBalance.multiply(fixedOwed).fromFixed().safeIntToUint();
+            .safeUintToInt().newFixed(this.decimals()); // owedDividendsPerToken is already a Fixidity number, remove the .newFixed(this.decimals()) extra conversion
+        return fixedBalance.multiply(fixedOwed).fromFixed().safeIntToUint(); // To convert back from fixidity to ether/wei is .fromFixed(this.decimals())
     }
 
 }
