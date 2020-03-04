7718851d960542ebf55e8d5dbc085e1d399a2cb6
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-03-04 11:59:34 +0000

    Extracted proposal logic into reusable contract.

---
+++ contracts/voting/Democratic.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.5.10;
+import "./../voting/Voting.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/EnumerableSet.sol";
+
+
+/**
+ * @title Democratic
+ * @author Alberto Cuesta Canada
+ * @dev Implements a module to add voting-based actions to other contracts.
+ */
+contract Democratic {
+    event Proposal(address proposal);
+
+    EnumerableSet.AddressSet internal proposals;
+    IERC20 public votingToken;
+    uint256 public threshold;
+
+    constructor (address _votingToken, uint256 _threshold)
+        public
+    {
+        votingToken = IERC20(_votingToken);
+        threshold = _threshold;
+    }
+
+    /// @dev Restricted to proposals. Same proposal cannot be used twice.
+    modifier onlyProposal() {
+        require(proposals.contains(msg.sender), "Restricted to proposals.");
+        _;
+        proposals.remove(msg.sender);
+    }
+
+    /// @dev Propose a democratic action.
+    /// @param proposalData The abi encoding of the proposal, as one function of this contract and any parameters.
+    function propose(
+        bytes memory proposalData
+    ) public {
+        Voting voting = new Voting(
+            address(votingToken),
+            address(this),
+            proposalData,
+            threshold
+        );
+        proposals.add(address(voting));
+        emit Proposal(address(voting));
+    }
+}
