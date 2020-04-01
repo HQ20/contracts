0dbccc345f0530ded001f2091237cbb13474d0e7
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-04-01 21:01:02 +0100

    Refactored Community.sol to use AccessControl.sol

--- contracts/access/Community.sol
+++ contracts/access/Community.sol
@@ -1,20 +1,17 @@
 pragma solidity ^0.6.0;
-import "./Roles.sol";
-import "./Renounceable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 
 /**
  * @title Community
  * @author Alberto Cuesta Canada
- * @notice Implements a single role Roles
+ * @notice Implements a single role access control contract.
  */
-contract Community is Roles, Renounceable {
-
-    bytes32 public constant COMMUNITY_ROLE_ID = "";
+contract Community is AccessControl {
 
     /// @dev Create the community role, with `root` as a member.
     constructor (address root) public {
-        _addMember(root, COMMUNITY_ROLE_ID);
+        _grantRole(DEFAULT_ADMIN_ROLE, root);
     }
 
     /// @dev Restricted to members of the community.
@@ -25,16 +22,16 @@ contract Community is Roles, Renounceable {
 
     /// @dev Return `true` if the `account` belongs to the community.
     function isMember(address account) public virtual view returns (bool) {
-        return hasRole(account, COMMUNITY_ROLE_ID);
+        return hasRole(DEFAULT_ADMIN_ROLE, account);
     }
 
     /// @dev Add a member of the community. Caller must already belong to the community.
     function addMember(address account) public virtual onlyMember {
-        _addMember(account, COMMUNITY_ROLE_ID);
+        _grantRole(DEFAULT_ADMIN_ROLE, account);
     }
 
     /// @dev Remove oneself as a member of the community.
     function leaveCommunity() public virtual { // Roles will check membership.
-        renounceMembership(COMMUNITY_ROLE_ID);
+        _revokeRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 }
