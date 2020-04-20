dc2076d5bdd4106fb720484666548141084ee8b5
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-04-20 20:33:22 +0100

    Fixed ERC20Mintable and ERC721Mintable.

--- contracts/token/ERC20Mintable.sol
+++ contracts/token/ERC20Mintable.sol
@@ -8,15 +8,14 @@ import "../access/Administered.sol";
  * @dev Extension of {ERC20} that gives the owner permission to mint (create) new tokens as he sees fit.
  */
 contract ERC20Mintable is ERC20, Administered {
-    constructor(string memory name, string memory symbol, uint8 decimals)
-        public ERC20(name, symbol, decimals) Administered(msg.sender)
-    {}
+    constructor(string memory name_, string memory symbol_, uint8 decimals_)
+        public ERC20(name_, symbol_) Administered(msg.sender)
+    {
+        _decimals = decimals_;
+    }
 
     function mint(address account, uint256 amount)
-        public
-        virtual
-        onlyAdmin
-        returns (bool)
+        public virtual onlyAdmin returns (bool)
     {
         _mint(account, amount);
         return true;
