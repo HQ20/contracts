f110a11fddf1911d910d26c516dbf0a4d84720ca
Alberto Cuesta Cañada <38806121+albertocuestacanada@users.noreply.github.com>
2020-06-05 11:06:09 +0100

    Implemented rounding division. (#349)
    * Implemented rounding towards zero division.
    
    * Implemented rounding away from zero division.

--- contracts/test/math/DecimalMathMock.sol
+++ contracts/test/math/DecimalMathMock.sol
@@ -66,4 +66,52 @@ contract DecimalMathMock {
     {
         return x.divd(y, decimals);
     }
+
+    function divdr(uint256 x, uint256 y)
+        public virtual pure returns (uint256)
+    {
+        return x.divdr(y);
+    }
+
+    function divdrInt(int256 x, int256 y)
+        public virtual pure returns (int256)
+    {
+        return x.divdr(y);
+    }
+
+    function divdr2(uint256 x, uint256 y, uint8 decimals)
+        public virtual pure returns (uint256)
+    {
+        return x.divdr(y, decimals);
+    }
+
+    function divdr2Int(int256 x, int256 y, uint8 decimals)
+        public virtual pure returns (int256)
+    {
+        return x.divdr(y, decimals);
+    }
+
+    function divdrup(uint256 x, uint256 y)
+        public virtual pure returns (uint256)
+    {
+        return x.divdrup(y);
+    }
+
+    function divdrupInt(int256 x, int256 y)
+        public virtual pure returns (int256)
+    {
+        return x.divdrup(y);
+    }
+
+    function divdrup2(uint256 x, uint256 y, uint8 decimals)
+        public virtual pure returns (uint256)
+    {
+        return x.divdrup(y, decimals);
+    }
+
+    function divdrup2Int(int256 x, int256 y, uint8 decimals)
+        public virtual pure returns (int256)
+    {
+        return x.divdrup(y, decimals);
+    }
 }
